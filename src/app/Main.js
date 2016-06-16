324a578474b21abd561b3e5f6f1a9fa2013a1684
Agniva De Sarker <agnivade@yahoo.co.in>
2016-06-16 22:36:29 +0530

    Added some basic UI elements

--- src/app/Main.js
+++ src/app/Main.js
@@ -3,8 +3,11 @@
  * which incorporates components providedby material-ui.
  */
 import React from 'react';
+
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import RefreshIndicator from 'material-ui/RefreshIndicator';
 import {deepOrange500} from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -12,15 +15,20 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 const styles = {
   container: {
     textAlign: 'center',
-    paddingTop: 50
+    paddingTop: 50,
+  },
+  paper: {
+    height: 100,
+    width: 600,
+    margin: '0 auto',
+    marginTop: '50px',
+  },
+  refresh: {
+    position: 'relative',
   },
 };
 
-const muiTheme = getMuiTheme({
-  palette: {
-    accent1Color: deepOrange500,
-  },
-});
+const muiTheme = getMuiTheme();
 
 class Main extends React.Component {
   constructor(props, context) {
@@ -30,9 +38,7 @@ class Main extends React.Component {
   }
 
   handleTouchTap() {
-    this.setState({
-      open: true,
-    });
+    console.log("clicked !");
   }
 
   render() {
@@ -41,6 +47,7 @@ class Main extends React.Component {
         <div style={styles.container}>
           <h1>Quick Word Lookup</h1>
           <TextField
+            ref="searchText"
             floatingLabelText="Enter the word you are looking for"
             fullWidth={true}
           />
@@ -48,6 +55,18 @@ class Main extends React.Component {
             label="Bring the Answer!"
             secondary={true}
             onTouchTap={this.handleTouchTap}
+          />
+          <Paper
+            children={<div>Store the response here</div>}
+            style={paperStyle}
+            zDepth={2}
+          />
+          <RefreshIndicator
+            size={40}
+            left={10}
+            top={0}
+            status="loading"
+            style={styles.refresh}
           />
         </div>
       </MuiThemeProvider>
